e84079cb6e35a2d2072716e3ca420c11baf1c3f5
tatui1 <kamila.lumazova@compasscollege.art>
2026-04-19 01:57:37 +0600

    использую Material UI

--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -1,7 +1,28 @@
+import { useState, type FormEvent,  } from 'react';
+import { TextField, Button, Paper, Stack } from '@mui/material';
+import { useChatStore } from '../../ChatStore';
+
 export const ChatForm = () => {
+    const addMessage = useChatStore(state => state.addMessage)
+    const [form, setForm] = useState({ author: '', message: '' })
+
+    const onSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        if (form.author.trim() && form.message.trim()) {
+            await addMessage(form)
+            setForm({ ...form, message: '' })
+        }
+    }
+
     return (
-        <div>
-            Chat Form
-        </div>
+        <Paper sx={{ p: 2, mb: 3 }}>
+            <Stack component="form" onSubmit={onSubmit} spacing={2}>
+                <TextField label="Author" variant="outlined" fullWidth 
+                    value={form.author} onChange={e => setForm({...form, author: e.target.value})} required />
+                <TextField label="Message" variant="outlined" fullWidth multiline rows={2}
+                    value={form.message} onChange={e => setForm({...form, message: e.target.value})} required />
+                <Button type="submit" variant="contained">Send</Button>
+            </Stack>
+        </Paper>
     )
 }
