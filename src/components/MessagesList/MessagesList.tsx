b6a0544fd33e979f101dfea156bf5beee67f56c7
tatui1 <kamila.lumazova@compasscollege.art>
2026-04-19 02:27:30 +0600

    работа лайков

--- src/components/MessagesList/MessagesList.tsx
+++ src/components/MessagesList/MessagesList.tsx
@@ -3,54 +3,49 @@ import type {ApiMessage, IMessageFull} from '../../types.ts'
 import {axiosApi} from '../../axiosApi.ts'
 import { Card, CardContent, Typography, Box, IconButton, Badge } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-
+import { useChatStore } from '../../ChatStore'; 
 
 export const MessagesList = () => {
     const [messages, setMessages] = useState<IMessageFull[]>([]);
-    const likeMessage = async (msg: IMessageFull) => {
-        const updatedLikes = (msg.likes || 0) + 1;
-        await axiosApi.put(`/messages/${msg.id}.json`, {
-            ...msg,
-            likes: updatedLikes
-        })
+    
+    const likeStoreMessage = useChatStore(state => state.likeMessage)
+
+    const getMessages = async() => {
+        try {
+            const response = await axiosApi<ApiMessage>('/messages.json')
+            const data = response.data
+            if (!data) return
+
+            const newMessages: IMessageFull[] = Object.keys(data).map(key => ({
+                ...data[key],
+                id: key, 
+            }))
+            setMessages(newMessages)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     useEffect(() => {
-        const getMessages = async() => {
-            try {
-                const response = await axiosApi<ApiMessage>('/messages.json');
-                const data = response.data;
-
-                if (!data) return;
-
-                const newMessages: IMessageFull[] = Object.keys(data).map(key => {
-                    const newMessage = data[key];
-                    return {
-                        ...newMessage,
-                        id: key, 
-                    }
-                })
-                setMessages(newMessages);
-            } catch (e) {
-                console.log(e);
-            }
-        }
-
         getMessages()
-
         const interval = setInterval(getMessages, 30000)
         return () => clearInterval(interval)
     }, [])
+
+    const handleLike = async (msg: IMessageFull) => {
+        await likeStoreMessage(msg)
+        await getMessages()
+    }
     
     return (
-        <div>
+        <Box sx={{ width: '100%', maxWidth: '600px' }}> 
             {messages.map(message => (
                 <Card key={message.id} variant="outlined" sx={{ mb: 2, borderRadius: '20px' }}>
                     <CardContent>
                         <Typography variant="h6">author: {message.author}</Typography>
                         <Typography variant="body1">{message.message}</Typography>
                         <Box sx={{ mt: 1 }}>
-                            <IconButton onClick={() => likeMessage(message)} color="error">
+                            <IconButton onClick={() => handleLike(message)} color="error">
                                 <Badge badgeContent={message.likes || 0} color="primary">
                                     <FavoriteIcon />
                                 </Badge>
@@ -59,6 +54,6 @@ export const MessagesList = () => {
                     </CardContent>
                 </Card>
             ))}
-        </div>
+        </Box>
     )
 }
